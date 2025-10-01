19a81433ca7cf383852b3e2327fe40cc4c959595
Thinhdang <thinhdang@Thinhdangs-MacBook-Pro.local>
2025-10-01 11:53:04 +0700

    Done

--- BT/src/App.jsx
+++ BT/src/App.jsx
@@ -1,28 +1,29 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Home from "./Pages/Home/Home";
 import UserPageMaster from "./Pages/PageMasters/UserPageMaster";
 
 const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <>
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={true} position="bottom-right" />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<UserPageMaster />}>
               <Route index element={<Home />} />
-              <Route path="/edituser/:id" element={<Home />} />
+              <Route path="edituser/:id" element={<Home />} />
             </Route>
           </Routes>
-        </QueryClientProvider>
-      </Provider>
-    </>
+        </BrowserRouter>
+
+        <ReactQueryDevtools initialIsOpen={true} position="bottom-right" />
+      </QueryClientProvider>
+    </Provider>
   );
 };
 
